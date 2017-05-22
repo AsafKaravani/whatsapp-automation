51e203dbfa1c72b94c9907b7439e29132ff6a988
Yuval <Yuval@DESKTOP-5MDMCNU>
2017-05-22 21:19:22 +0300

    Add convert image to base64 (need to check...)

--- src/main/java/boot/SingleConversationBehavior.java
+++ src/main/java/boot/SingleConversationBehavior.java
@@ -1,11 +1,24 @@
 package boot;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.IOUtils;
 import org.openqa.selenium.WebElement;
 
 public class SingleConversationBehavior extends WhatsAppBehaviors {
@@ -22,7 +35,7 @@ public class SingleConversationBehavior extends WhatsAppBehaviors {
 	 */
 	public SingleConversationBehavior(){
 		namesWithImageMap = new HashMap<String, String>();
-		contactNames = Arrays.asList("עד מתי 2014","גיא גולן");
+		contactNames = Arrays.asList("אסף לימודים");
 		i = 0;
 	}
 	/*
@@ -40,12 +53,26 @@ public class SingleConversationBehavior extends WhatsAppBehaviors {
 				answer.click();
 				String answerImage = whatsapp.getCurrentConvImg();
 				if(!answerImage.startsWith("data")){
-					String msgName = whatsapp.getCurrentConvName();
-					if(namesWithImageMap.get(msgName) == null)
+					String msgName = whatsapp.getCurrentConvName(); 
+					if(namesWithImageMap.get(msgName) == null){
 						namesWithImageMap.put(msgName, answerImage);
+						byte[] imageBytes = null;
+						try {
+							imageBytes = IOUtils.toByteArray(new URL(answerImage));
+						} catch (MalformedURLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						String base64 = Base64.getEncoder().encodeToString(imageBytes);
+					    System.out.println("base64-==="+base64);
+					}
 				}
 			}else{
 				openConversation();
+				
 			}
 			try {
 				TimeUnit.SECONDS.sleep(10);
@@ -53,6 +80,7 @@ public class SingleConversationBehavior extends WhatsAppBehaviors {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			System.out.println(namesWithImageMap);
 		}
 	}
 	/*
@@ -67,6 +95,14 @@ public class SingleConversationBehavior extends WhatsAppBehaviors {
 			String img = whatsapp.getCurrentConvImg();
 			if(!img.startsWith("data")){
 				namesWithImageMap.put(contactNames.get(i), img);
+				System.out.println("answerImage==="+img);
+				Image image = null;
+				try {
+					byte[] imageBytes = IOUtils.toByteArray(new URL(img));
+					String base64 = Base64.getEncoder().encodeToString(imageBytes);
+				    System.out.println("base64-==="+base64);
+				} catch (IOException e) {
+				}
 			}else sendMessage();
 			i++;
 		}
@@ -78,7 +114,7 @@ public class SingleConversationBehavior extends WhatsAppBehaviors {
 	 */
 	@Override
 	public void sendMessage() {
-		whatsapp.sendMsg("TEST");
+		whatsapp.sendMsg("מה נשמע?");
 	}
 	/*
 	 * (non-Javadoc)
